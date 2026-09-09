f25d5dd9d1b70c60d2ef6ecf9be99b1266e09232
Vinuu's Github <vinayakkukkar11@gmail.com>
2026-09-09 19:22:08 +0530

    Strip embedded credentials from PostgreSQL JDBC URL in DatabaseUrlSanitizer

--- backend/src/main/java/com/siddhi/paithani/config/DatabaseUrlSanitizer.java
+++ backend/src/main/java/com/siddhi/paithani/config/DatabaseUrlSanitizer.java
@@ -26,26 +26,57 @@ public class DatabaseUrlSanitizer implements EnvironmentPostProcessor {
 
         if (dbUrl != null && !dbUrl.isBlank()) {
             String sanitizedUrl = dbUrl.trim();
-            if (sanitizedUrl.startsWith("postgres://")) {
-                sanitizedUrl = "jdbc:postgresql://" + sanitizedUrl.substring("postgres://".length());
-            } else if (sanitizedUrl.startsWith("postgresql://")) {
-                sanitizedUrl = "jdbc:postgresql://" + sanitizedUrl.substring("postgresql://".length());
-            } else if (sanitizedUrl.startsWith("mysql://")) {
-                sanitizedUrl = "jdbc:mysql://" + sanitizedUrl.substring("mysql://".length());
+
+            if (sanitizedUrl.startsWith("jdbc:")) {
+                sanitizedUrl = sanitizedUrl.substring(5);
             }
 
-            props.put("spring.datasource.url", sanitizedUrl);
+            String scheme = "";
+            if (sanitizedUrl.startsWith("postgres://")) {
+                scheme = "postgresql://";
+                sanitizedUrl = sanitizedUrl.substring("postgres://".length());
+            } else if (sanitizedUrl.startsWith("postgresql://")) {
+                scheme = "postgresql://";
+                sanitizedUrl = sanitizedUrl.substring("postgresql://".length());
+            } else if (sanitizedUrl.startsWith("mysql://")) {
+                scheme = "mysql://";
+                sanitizedUrl = sanitizedUrl.substring("mysql://".length());
+            } else if (sanitizedUrl.contains("://")) {
+                int idx = sanitizedUrl.indexOf("://");
+                scheme = sanitizedUrl.substring(0, idx + 3);
+                sanitizedUrl = sanitizedUrl.substring(idx + 3);
+            }
+
+            // Extract embedded user:password@host if present
+            if (sanitizedUrl.contains("@")) {
+                int atIdx = sanitizedUrl.indexOf("@");
+                String userInfo = sanitizedUrl.substring(0, atIdx);
+                String hostAndDb = sanitizedUrl.substring(atIdx + 1);
+
+                if (userInfo.contains(":")) {
+                    String[] userPass = userInfo.split(":", 2);
+                    props.put("spring.datasource.username", userPass[0]);
+                    props.put("spring.datasource.password", userPass[1]);
+                } else {
+                    props.put("spring.datasource.username", userInfo);
+                }
+
+                sanitizedUrl = hostAndDb;
+            }
+
+            String finalJdbcUrl = "jdbc:" + scheme + sanitizedUrl;
+            props.put("spring.datasource.url", finalJdbcUrl);
 
             // Dynamically set Driver and Dialect based on URL type to prevent metadata lookup failures
-            if (sanitizedUrl.contains("postgresql")) {
+            if (finalJdbcUrl.contains("postgresql")) {
                 props.put("spring.datasource.driver-class-name", "org.postgresql.Driver");
                 props.put("spring.jpa.database-platform", "org.hibernate.dialect.PostgreSQLDialect");
                 props.put("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-            } else if (sanitizedUrl.contains("mysql")) {
+            } else if (finalJdbcUrl.contains("mysql")) {
                 props.put("spring.datasource.driver-class-name", "com.mysql.cj.jdbc.Driver");
                 props.put("spring.jpa.database-platform", "org.hibernate.dialect.MySQLDialect");
                 props.put("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-            } else if (sanitizedUrl.contains("h2")) {
+            } else if (finalJdbcUrl.contains("h2")) {
                 props.put("spring.datasource.driver-class-name", "org.h2.Driver");
                 props.put("spring.jpa.database-platform", "org.hibernate.dialect.H2Dialect");
                 props.put("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.H2Dialect");
